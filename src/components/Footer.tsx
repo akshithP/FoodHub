380e11e74dbcdab5f5251f084c317d6a3e022747
Akshith-Patil <akshithp13@gmail.com>
2024-01-12 10:49:47 +1100

    Changed title and fixed countdown

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
-      <Link href="/" className="font-bold text-xl">MASSIMO</Link>
-      <p>© ALL RIGHTS RESERVED.</p>
+      <Link href="/" className="font-bold text-xl">
+        FOODHUB
+      </Link>
+      <p>©By Akshith Patil.</p>
     </div>
   );
 };
